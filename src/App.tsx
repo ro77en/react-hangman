85cf2302aee509195d837d1fc43fb841e4ebd0b3
ro77en <lucas.mdantas@icloud.com>
2024-08-27 14:17:43 -0300

    render hangman on keypress

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import words from "./wordList.json";
 import { HangmanDrawing } from "./components/HangmanDrawing";
@@ -11,7 +11,36 @@ function App() {
   });
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
-  const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter));
+  const incorrectLetters = guessedLetters.filter(
+    (letter) => !wordToGuess.includes(letter)
+  );
+
+  const addGuessedLetter = useCallback((letter: string) => {
+    if (guessedLetters.includes(letter)) return;
+
+    setGuessedLetters(currentLetters => [...currentLetters, letter]);
+
+  }, [guessedLetters])
+
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+      console.log(key)
+      addGuessedLetter(key);
+    }
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    }
+
+  }, [guessedLetters]);
 
   console.log(wordToGuess);
 
