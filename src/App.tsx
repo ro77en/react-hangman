4f1b7e3714add51d786f343cde2485f627ce2d34
ro77en <lucas.mdantas@icloud.com>
2024-08-27 13:31:18 -0300

    main code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import words from "./wordList.json";
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord";
+import { Keyboard } from "./components/Keyboard";
 
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
 
-  return <h1>hi</h1>
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
+
+  return (
+    <div className="container">
+      <div className="showResult">Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
