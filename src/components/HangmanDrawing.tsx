59431b4a4f3171f3f0088f0d83874a351246c799
ro77en <lucas.mdantas@icloud.com>
2024-08-27 13:42:51 -0300

    set up HangmanDrawingProps

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -2,20 +2,21 @@ import "./HangmanDrawing.css";
 
 const HEAD = <div className="head" />;
 const BODY = <div className="body" />;
-const RIGHT_ARM = <div className="right arm" />
-const LEFT_ARM = <div className="left arm" />
-const RIGHT_LEG = <div className="right leg" />
-const LEFT_LEG = <div className="left leg" />
+const RIGHT_ARM = <div className="right arm" />;
+const LEFT_ARM = <div className="left arm" />;
+const RIGHT_LEG = <div className="right leg" />;
+const LEFT_LEG = <div className="left leg" />;
 
-export function HangmanDrawing() {
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProps = {
+  numberOfGuesses: number;
+};
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
   return (
     <div className="drawing">
-      {HEAD}
-      {BODY}
-      {RIGHT_ARM}
-      {LEFT_ARM}
-      {RIGHT_LEG}
-      {LEFT_LEG}
+      {BODY_PARTS.slice(0, numberOfGuesses)}
       <div className="top-right line" />
       <div className="top line" />
       <div className="middle line" />
