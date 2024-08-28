c1da6a84e035540ff92a1bec220ea9185383c42e
ro77en <lucas.mdantas@icloud.com>
2024-08-28 10:04:38 -0300

    reveals word when user loses

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,21 +1,22 @@
 import "./HangmanWord.css";
 
 type HangmanWordProps = {
-  guessedLetters: string[],
-  wordToGuess: string
-}
+  guessedLetters: string[];
+  wordToGuess: string;
+  reveal?: boolean;
+};
 
-export function HangmanWord({guessedLetters, wordToGuess}: HangmanWordProps) {
-
+export function HangmanWord({ guessedLetters, wordToGuess, reveal = false }: HangmanWordProps) {
   return (
     <div className="word">
       {wordToGuess.split("").map((letter, index) => (
         <span className="letter-border" key={index}>
           <span
             style={{
-              visibility: guessedLetters.includes(letter)
+              visibility: guessedLetters.includes(letter) || reveal
                 ? "visible"
                 : "hidden",
+                color: !guessedLetters.includes(letter) && reveal ? 'red' : 'white'
             }}
           >
             {letter}
